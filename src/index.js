3ec823693c93e8c6ca19d199613a601ff6279d88
Emine Shaban <emineshaban@icloud.com>
2022-09-02 11:36:02 +0300

    add homecontroller and router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-
+const routes = require('./routes')
 const app =express()
 
 app.use('/static', express.static('public'))
@@ -11,7 +11,5 @@ app.engine('hbs', handlebars.engine({
 
 app.set('view engine', 'hbs')
 app.set('views', './src/views')
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use(routes)
 app.listen(5001, () => console.log('App is listening on port 5001'))    
