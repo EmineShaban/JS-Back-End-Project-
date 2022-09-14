0ac80d52ac9b5085710ef2711e102b16d3b12fed
Emine Shaban <emineshaban@icloud.com>
2022-09-14 12:13:58 +0300

    Create accessory model

---
+++ src/models/Accessory.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const accessorySchema = new mongoose.Schema({
+    // _id: mongoose.Types.ObjectId,
+    name: {
+        type: String,
+        require: true,
+        // unique: true,
+    },
+    imageUrl: {
+        type: String,
+        require: true,
+        validate: {
+            validator: /^http?/g,
+            message: "Image url should be a link"
+        }
+    },
+    description: {
+        type: String,
+        require: true,
+        maxlength: 120,
+    }
+
+
+})
+
+// cubeSchema.path('imageUrl').validate(function () {
+// return this.imageUrl.startsWith('http')
+// }, 'Image url should be a link')
+
+const Accessory = mongoose.model('Accessory', accessorySchema)
+
+module.exports = Accessory
